750f2ee269437eb7ea17a0e6af58abfd0fdce1da
Foysal79 <onthor312001@gmail.com>
2023-11-28 03:41:34 +0600

    user info all mongodb to send client side

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+
+    } )
+
 
 
     // Send a ping to confirm a successful connection
